e1413a79f98b80f1607c3263a842301fd1377a38
java <java>
2017-09-05 15:13:05 +0900

    java version 1.5 -> 1.8

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -1,5 +1,12 @@
 package com.example;
 
 public class HelloSpring {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("Spring의 두가지 .... 설계");
+	System.out.println("1. DI(Dependency Injection)");
+	System.out.println("2. AOP(Aspect Objected Programming");
 
+	}
 }
